dc21d2b5a2a3e86dbe1f9c1f828234c7fa9a22e4
ezch74 <charlesyagazie95@gmail.com>
2025-01-13 07:19:59 +0100

    ispan

--- main.js
+++ main.js
@@ -22,3 +22,16 @@ console.log(Math.sign(num))
 // console.log("Hello World");
 
 // console.log(reverse('yams'));
+
+    let atoz = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
+function isPangram(str){
+    let strArr = str.toLowerCase().split('');
+    let alphabets = atoz.split(',');
+    let result = true;
+    alphabets.forEach(letter => {
+        if(strArr.indexOf(letter) === -1){
+            result = false
+        }
+    })
+    return result;
+}
